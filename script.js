d241fea35c661d520543716523b7eb4ea8587bdf
yongbioinfo <99473204+yongbioinfo@users.noreply.github.com>
2023-11-02 17:38:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,25 +26,25 @@ function getCountdown(targetHour, targetMinute) {
 
 function updateCountdown() {
     const usMarketCountdown = document.querySelector('.us-market .countdown');
-    usMarketCountdown.textContent = getCountdown(9, 30);  // US market opens at 9:30
+    usMarketCountdown.textContent = getCountdown(9, 30);
 
-    // 다른 나라의 카운트다운도 유사한 방식으로 추가
-
-    setTimeout(updateCountdown, 1000);  // 매 초마다 업데이트
+    setTimeout(updateCountdown, 1000);
 }
 
-function showKoreaTime() {
-    const now = new Date();
-    // 한국 시간으로 조정
-    const koreaTime = new Date(now.getTime() + (9 * 60 * 60 * 1000));
-    const hours = koreaTime.getHours();
-    const minutes = koreaTime.getMinutes();
-    const formattedTime = `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+document.addEventListener('DOMContentLoaded', function() {
+    function updateKoreanTime() {
+        const now = new Date();
+        const koreaTime = new Date(now.getTime() + (9 * 60 * 60 * 1000));
+        const hours = koreaTime.getHours();
+        const minutes = koreaTime.getMinutes();
+        const seconds = koreaTime.getSeconds();
+        
+        const formattedTime = `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
 
-    document.getElementById('koreaTime').innerText = `한국 시간: ${formattedTime}`;
-}
+        document.getElementById('koreaTime').innerText = `한국 시간: ${formattedTime}`;
+    }
 
-// 처음 로드 시 한 번 호출하고, 1분마다 갱신
-showKoreaTime();
-setInterval(showKoreaTime, 60000);
-updateCountdown();
+    updateKoreanTime();
+    setInterval(updateKoreanTime, 1000);
+    updateCountdown();
+});
